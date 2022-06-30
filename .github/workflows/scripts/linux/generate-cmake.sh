0f202e75047d2c62f6e032d7455dddce08c13bb9
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2022-06-30 08:21:11 -0700

    Fix tab-spacing

--- .github/workflows/scripts/linux/generate-cmake.sh
+++ .github/workflows/scripts/linux/generate-cmake.sh
@@ -11,13 +11,13 @@ fi
 echo "Additional CMake Args - ${ADDITIONAL_CMAKE_ARGS}"
 
 # Generate CMake into ./build
-cmake                                     	\
--DCMAKE_BUILD_TYPE=Release		          	  \
+cmake                                       \
+-DCMAKE_BUILD_TYPE=Release                  \
 -DCMAKE_INSTALL_PREFIX="squashfs-root/usr/"	\
 -DCMAKE_PREFIX_PATH="$HOME/Depends"        	\
 -DWAYLAND_API=ON                           	\
 -DXDG_STD=TRUE                             	\
--DDISABLE_ADVANCE_SIMD=TRUE					        \
+-DDISABLE_ADVANCE_SIMD=TRUE	                \
 -DUSE_VULKAN=ON                            	\
 -DPACKAGE_MODE=ON                          	\
 -DDISABLE_SETCAP=ON                        	\
