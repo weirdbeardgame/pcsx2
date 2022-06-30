ba3384cf8ab20f2be07912c448915691475e2d50
Live session user <qurious@gmx.com>
2022-06-30 06:00:34 -0700

    [CI] Build Qt and wxWidgets

--- .github/workflows/scripts/linux/generate-cmake.sh
+++ .github/workflows/scripts/linux/generate-cmake.sh
@@ -5,16 +5,16 @@ set -e
 echo "Additional CMake Args - ${ADDITIONAL_CMAKE_ARGS}"
 
 # Generate CMake into ./build
-cmake                                     \
--DCMAKE_BUILD_TYPE=RelWithDebInfo          \
--DCMAKE_INSTALL_PREFIX="squashfs-root/usr/" \
--DCMAKE_PREFIX_PATH="$HOME/Depends"          \
--DWAYLAND_API=ON                              \
--DQT_BUILD=ON                                  \
--DXDG_STD=TRUE                                  \
--DDISABLE_ADVANCE_SIMD=TRUE						 \
--DUSE_VULKAN=ON                                   \
--DPACKAGE_MODE=ON                                  \
--DDISABLE_SETCAP=ON                                 \
--GNinja                                              \
+cmake                                     	\
+-DCMAKE_BUILD_TYPE=Release		          	\
+-DCMAKE_INSTALL_PREFIX="squashfs-root/usr/"	\
+-DCMAKE_PREFIX_PATH="$HOME/Depends"        	\
+-DWAYLAND_API=ON                           	\
+-DXDG_STD=TRUE                             	\
+-DDISABLE_ADVANCE_SIMD=TRUE					\
+-DUSE_VULKAN=ON                            	\
+-DPACKAGE_MODE=ON                          	\
+-DDISABLE_SETCAP=ON                        	\
+"$ADDITIONAL_CMAKE_ARGS"                   	\
+-GNinja                                    	\
 -B build
