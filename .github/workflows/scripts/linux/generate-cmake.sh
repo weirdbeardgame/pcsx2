284a876fa037b3ebdb4df53f0eed560dc787b489
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2022-06-30 08:19:46 -0700

    build based on compiler

--- .github/workflows/scripts/linux/generate-cmake.sh
+++ .github/workflows/scripts/linux/generate-cmake.sh
@@ -2,16 +2,22 @@
 
 set -e
 
+if [ "${COMPILER}" = "gcc" ]; then
+  export CC=gcc-10; export CXX=g++-10
+else
+  export CC=clang; export CXX=clang++
+fi
+
 echo "Additional CMake Args - ${ADDITIONAL_CMAKE_ARGS}"
 
 # Generate CMake into ./build
 cmake                                     	\
--DCMAKE_BUILD_TYPE=Release		          	\
+-DCMAKE_BUILD_TYPE=Release		          	  \
 -DCMAKE_INSTALL_PREFIX="squashfs-root/usr/"	\
 -DCMAKE_PREFIX_PATH="$HOME/Depends"        	\
 -DWAYLAND_API=ON                           	\
 -DXDG_STD=TRUE                             	\
--DDISABLE_ADVANCE_SIMD=TRUE					\
+-DDISABLE_ADVANCE_SIMD=TRUE					        \
 -DUSE_VULKAN=ON                            	\
 -DPACKAGE_MODE=ON                          	\
 -DDISABLE_SETCAP=ON                        	\
