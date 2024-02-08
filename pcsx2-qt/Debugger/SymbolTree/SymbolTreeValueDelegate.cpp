c1f9ac190d840135d943be6c80adc4c819aad3ac
chaoticgd <43898262+chaoticgd@users.noreply.github.com>
2024-02-08 02:19:29 +0000

    Debugger: Use QLineEdits for inputting numbers in the symbol tree
    Previously I was using custom spin boxes, but I think this is a
    better solution.

--- pcsx2-qt/Debugger/SymbolTree/SymbolTreeValueDelegate.cpp
+++ pcsx2-qt/Debugger/SymbolTree/SymbolTreeValueDelegate.cpp
@@ -7,7 +7,6 @@
 #include <QtWidgets/QComboBox>
 #include <QtWidgets/QDoubleSpinBox>
 #include <QtWidgets/QLineEdit>
-#include "Int64SpinBox.h"
 #include "Debugger/SymbolTree/SymbolTreeModel.h"
 
 SymbolTreeValueDelegate::SymbolTreeValueDelegate(
@@ -41,40 +40,20 @@ QWidget* SymbolTreeValueDelegate::createEditor(QWidget* parent, const QStyleOpti
 				switch (builtIn.bclass)
 				{
 					case ccc::ast::BuiltInClass::UNSIGNED_8:
-						result = new UInt64SpinBox(0, UINT8_MAX, parent);
-						break;
 					case ccc::ast::BuiltInClass::SIGNED_8:
-						result = new Int64SpinBox(INT8_MIN, INT8_MAX, parent);
-						break;
 					case ccc::ast::BuiltInClass::UNQUALIFIED_8:
-						result = new UInt64SpinBox(0, UINT8_MAX, parent);
+					case ccc::ast::BuiltInClass::UNSIGNED_16:
+					case ccc::ast::BuiltInClass::SIGNED_16:
+					case ccc::ast::BuiltInClass::UNSIGNED_32:
+					case ccc::ast::BuiltInClass::SIGNED_32:
+					case ccc::ast::BuiltInClass::FLOAT_32:
+					case ccc::ast::BuiltInClass::UNSIGNED_64:
+					case ccc::ast::BuiltInClass::SIGNED_64:
+					case ccc::ast::BuiltInClass::FLOAT_64:
+						result = new QLineEdit(parent);
 						break;
 					case ccc::ast::BuiltInClass::BOOL_8:
 						result = new QCheckBox(parent);
-						break;
-					case ccc::ast::BuiltInClass::UNSIGNED_16:
-						result = new UInt64SpinBox(0, INT16_MAX, parent);
-						break;
-					case ccc::ast::BuiltInClass::SIGNED_16:
-						result = new Int64SpinBox(INT16_MIN, INT16_MAX, parent);
-						break;
-					case ccc::ast::BuiltInClass::UNSIGNED_32:
-						result = new UInt64SpinBox(0, UINT32_MAX, parent);
-						break;
-					case ccc::ast::BuiltInClass::SIGNED_32:
-						result = new Int64SpinBox(INT32_MIN, INT32_MAX, parent);
-						break;
-					case ccc::ast::BuiltInClass::FLOAT_32:
-						result = new QDoubleSpinBox(parent);
-						break;
-					case ccc::ast::BuiltInClass::UNSIGNED_64:
-						result = new UInt64SpinBox(0, UINT64_MAX, parent);
-						break;
-					case ccc::ast::BuiltInClass::SIGNED_64:
-						result = new Int64SpinBox(INT64_MIN, INT64_MAX, parent);
-						break;
-					case ccc::ast::BuiltInClass::FLOAT_64:
-						result = new QDoubleSpinBox(parent);
 						break;
 					default:
 					{
@@ -131,9 +110,11 @@ void SymbolTreeValueDelegate::setEditorData(QWidget* editor, const QModelIndex& 
 					case ccc::ast::BuiltInClass::UNSIGNED_32:
 					case ccc::ast::BuiltInClass::UNSIGNED_64:
 					{
-						UInt64SpinBox* spinBox = qobject_cast<UInt64SpinBox*>(editor);
-						Q_ASSERT(spinBox);
-						spinBox->setValue(index.data(Qt::UserRole).toULongLong());
+						QLineEdit* lineEdit = qobject_cast<QLineEdit*>(editor);
+						Q_ASSERT(lineEdit);
+
+						lineEdit->setText(QString::number(index.data(Qt::UserRole).toULongLong()));
+
 						break;
 					}
 					case ccc::ast::BuiltInClass::SIGNED_8:
@@ -141,30 +122,38 @@ void SymbolTreeValueDelegate::setEditorData(QWidget* editor, const QModelIndex& 
 					case ccc::ast::BuiltInClass::SIGNED_32:
 					case ccc::ast::BuiltInClass::SIGNED_64:
 					{
-						Int64SpinBox* spinBox = qobject_cast<Int64SpinBox*>(editor);
-						Q_ASSERT(spinBox);
-						spinBox->setValue(index.data(Qt::UserRole).toLongLong());
+						QLineEdit* lineEdit = qobject_cast<QLineEdit*>(editor);
+						Q_ASSERT(lineEdit);
+
+						lineEdit->setText(QString::number(index.data(Qt::UserRole).toLongLong()));
+
 						break;
 					}
 					case ccc::ast::BuiltInClass::BOOL_8:
 					{
 						QCheckBox* checkBox = qobject_cast<QCheckBox*>(editor);
 						Q_ASSERT(checkBox);
+
 						checkBox->setChecked(index.data(Qt::UserRole).toBool());
+
 						break;
 					}
 					case ccc::ast::BuiltInClass::FLOAT_32:
 					{
-						QDoubleSpinBox* spinBox = qobject_cast<QDoubleSpinBox*>(editor);
-						Q_ASSERT(spinBox);
-						spinBox->setValue(index.data(Qt::UserRole).toFloat());
+						QLineEdit* lineEdit = qobject_cast<QLineEdit*>(editor);
+						Q_ASSERT(lineEdit);
+
+						lineEdit->setText(QString::number(index.data(Qt::UserRole).toFloat()));
+
 						break;
 					}
 					case ccc::ast::BuiltInClass::FLOAT_64:
 					{
-						QDoubleSpinBox* spinBox = qobject_cast<QDoubleSpinBox*>(editor);
-						Q_ASSERT(spinBox);
-						spinBox->setValue(index.data(Qt::UserRole).toFloat());
+						QLineEdit* lineEdit = qobject_cast<QLineEdit*>(editor);
+						Q_ASSERT(lineEdit);
+
+						lineEdit->setText(QString::number(index.data(Qt::UserRole).toDouble()));
+
 						break;
 					}
 					default:
@@ -230,9 +219,14 @@ void SymbolTreeValueDelegate::setModelData(QWidget* editor, QAbstractItemModel* 
 					case ccc::ast::BuiltInClass::UNSIGNED_32:
 					case ccc::ast::BuiltInClass::UNSIGNED_64:
 					{
-						UInt64SpinBox* spinBox = qobject_cast<UInt64SpinBox*>(editor);
-						Q_ASSERT(spinBox);
-						model->setData(index, spinBox->value(), Qt::UserRole);
+						QLineEdit* lineEdit = qobject_cast<QLineEdit*>(editor);
+						Q_ASSERT(lineEdit);
+
+						bool ok;
+						qulonglong value = lineEdit->text().toULongLong(&ok);
+						if (ok)
+							model->setData(index, value, Qt::UserRole);
+
 						break;
 					}
 					case ccc::ast::BuiltInClass::SIGNED_8:
@@ -240,29 +234,45 @@ void SymbolTreeValueDelegate::setModelData(QWidget* editor, QAbstractItemModel* 
 					case ccc::ast::BuiltInClass::SIGNED_32:
 					case ccc::ast::BuiltInClass::SIGNED_64:
 					{
-						Int64SpinBox* spinBox = qobject_cast<Int64SpinBox*>(editor);
-						Q_ASSERT(spinBox);
-						model->setData(index, spinBox->value(), Qt::UserRole);
+						QLineEdit* lineEdit = qobject_cast<QLineEdit*>(editor);
+						Q_ASSERT(lineEdit);
+
+						bool ok;
+						qlonglong value = lineEdit->text().toLongLong(&ok);
+						if (ok)
+							model->setData(index, value, Qt::UserRole);
+
 						break;
 					}
 					case ccc::ast::BuiltInClass::BOOL_8:
 					{
 						QCheckBox* checkBox = qobject_cast<QCheckBox*>(editor);
 						model->setData(index, checkBox->isChecked(), Qt::UserRole);
+
 						break;
 					}
 					case ccc::ast::BuiltInClass::FLOAT_32:
 					{
-						QDoubleSpinBox* spinBox = qobject_cast<QDoubleSpinBox*>(editor);
-						Q_ASSERT(spinBox);
-						model->setData(index, static_cast<float>(spinBox->value()), Qt::UserRole);
+						QLineEdit* lineEdit = qobject_cast<QLineEdit*>(editor);
+						Q_ASSERT(lineEdit);
+
+						bool ok;
+						float value = lineEdit->text().toFloat(&ok);
+						if (ok)
+							model->setData(index, value, Qt::UserRole);
+
 						break;
 					}
 					case ccc::ast::BuiltInClass::FLOAT_64:
 					{
-						QDoubleSpinBox* spinBox = qobject_cast<QDoubleSpinBox*>(editor);
-						Q_ASSERT(spinBox);
-						model->setData(index, spinBox->value(), Qt::UserRole);
+						QLineEdit* lineEdit = qobject_cast<QLineEdit*>(editor);
+						Q_ASSERT(lineEdit);
+
+						bool ok;
+						double value = lineEdit->text().toDouble(&ok);
+						if (ok)
+							model->setData(index, value, Qt::UserRole);
+
 						break;
 					}
 					default:
